0b347f27b6d41f65e0744401d49d9446cbc94fb5
Nikita Li <nikitali@Mac.fritz.box>
2025-08-18 14:43:49 +0200

    change server url

--- config.py
+++ config.py
@@ -2,8 +2,7 @@ from cryptography.fernet import Fernet
 
 # Сгенерируй ключ один раз и вставь сюда
 SECRET_KEY = b'brcq1K-n7isVR68ZWJfa2BTG7lb6LoUH85s6SA6K-Nc='
-SERVER_URL = "http://127.0.0.1:8000"    # теперь локальный адрес
-# Токен твоего Telegram-бота
+SERVER_URL = "https://unfortunate-mandi-petprojects1-b97eb276.koyeb.app"# Токен твоего Telegram-бота
 BOT_TOKEN = "8119199682:AAF47W8rc0VgC6wDHfBhvAHcFC_lhO02gTA"
 IPQS_TOKEN = "<KEY>"
 IPINFO_TOKEN = "<KEY>"
